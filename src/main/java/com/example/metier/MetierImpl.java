76892042cfff6aa5a319462c397cbcb122df2719
AbdelilahBKM <boukhatem.abdelilah@outlook.com>
2025-10-14 22:01:45 +0100

    first commit

---
+++ src/main/java/com/example/metier/MetierImpl.java
@@ -0,0 +1,12 @@
+package com.example.metier;
+import com.example.DAO.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("metier")
+public class MetierImpl implements IMetier {
+    @Autowired
+    private IDao dao;
+    @Override
+    public double calcul() { return dao.getValue() * 2; }
+}
---
+++ src/main/java/com/example/presentation/Presentation2.java
@@ -0,0 +1,19 @@
+package com.example.presentation;
+
+import com.example.metier.IMetier;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(basePackages = {"dao","metier","config"})
+public class Presentation2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+
+        ctx.register(Presentation2.class);
+        ctx.refresh();
+
+        IMetier metier = ctx.getBean(IMetier.class);
+        System.out.println("Résultat = " + metier.calcul());
+        ctx.close();
+    }
+}
